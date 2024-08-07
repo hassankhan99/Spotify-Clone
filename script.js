01701c2437baeeaf1cae6828f52fa9ec61b769fa
hassankhan99 <waheedsahab024@gmail.com>
2024-08-06 22:54:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterplay = document.getElementById('masterplay');
+let gif = document.getElementById('gif');
+let MyProgresBar = document.getElementById('MyProgresBar');
+let songs = [
+    { songName: 'salam-e-Ishq', filePath: 'songs/1.mp3', coverPath: 'covers/1.jpg' },
+    { songName: 'Tum Hi Ho', filePath: 'songs/2.mp3', coverPath: 'covers/2.jpg' },
+    { songName: 'Channa Mereya', filePath: 'songs/3.mp3', coverPath: 'covers/3.jpg' },
+    { songName: 'Raabta', filePath: 'songs/4.mp3', coverPath: 'covers/4.jpg' },
+    { songName: 'Tera Ban Jaunga', filePath: 'songs/5.mp3', coverPath: 'covers/5.jpg' },
+    { songName: 'Tujhe Kitna Chahne Lage', filePath: 'songs/6.mp3', coverPath: 'covers/6.jpg' },
+    { songName: 'Kabira', filePath: 'songs/7.mp3', coverPath: 'covers/7.jpg' },
+    { songName: 'Janam Janam', filePath: 'songs/8.mp3', coverPath: 'covers/8.jpg' },
+    { songName: 'Ae Dil Hai Mushkil', filePath: 'songs/9.mp3', coverPath: 'covers/9.jpg' },
+    { songName: 'Zalima', filePath: 'songs/10.mp3', coverPath: 'covers/10.jpg' },
+];
+
+
+
+masterplay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterplay.classList.remove('fa-circle-play')
+        masterplay.classList.add('fa-circle-pause')
+        gif.style.opacity = 1;
+    } else {
+        audioElement.pause();
+        masterplay.classList.remove('fa-circle-pause')
+        masterplay.classList.add('fa-circle-play')
+        gif.style.opacity = 0;
+    }
+});
+
+audioElement.addEventListener('timeupdate', () => {
+    progres = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    MyProgresBar.value = progres;
+});
+MyProgresBar.addEventListener('change', () => {
+    audioElement.currentTime = MyProgresBar.value * audioElement.duration/100;
+})
+const makeplays = ()=>{
+    Array.from(document.getElementsByClassName('songItemsPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause')
+        element.classList.add('fa-circle-play')
+
+    })
+}
+Array.from(document.getElementsByClassName('songItemsPlay')).forEach((element)=>{
+    element.addEventListener('click' , (e)=>{
+        console.log(e.target);
+        makeplays();
+        e.target.classList.remove('fa-circle-play')
+        e.target.classList.add('fa-circle-pause')
+        
+    })
+
+    }
+)
+
